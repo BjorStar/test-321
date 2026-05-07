b561c3eed102d9cf9c4579c02de0fb6394acda26
Robin <100517796+BjorStar@users.noreply.github.com>
2026-05-07 14:30:33 +0300

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,20 +1,46 @@
-import os, psycopg
+import os
+import psycopg
+from psycopg.rows import dict_row
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 def get_conn():
-    return psycopg.connect(DATABASE_URL, autocommit=True, row_factory=psycopg.rows.dict_row)
+    return psycopg.connect(
+        DATABASE_URL,
+        autocommit=True,
+        row_factory=dict_row
+    )
 
 def create_schema():
     try:
         with get_conn() as conn, conn.cursor() as cur:
+
+            # USERS
             cur.execute("""
-            
-                CREATE TABLE IF NOT EXISTS todo_users (
+                CREATE TABLE IF NOT EXISTS users (
                     id SERIAL PRIMARY KEY,
-                    username VARCHAR NOT NULL
+                    api_key TEXT UNIQUE NOT NULL
                 );
-                """)
+            """)
+
+            # CATEGORIES
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS categories (
+                    id SERIAL PRIMARY KEY,
+                    name TEXT NOT NULL
+                );
+            """)
+
+            # TODOS
+            cur.execute("""
+                CREATE TABLE IF NOT EXISTS todos (
+                    id SERIAL PRIMARY KEY,
+                    text TEXT NOT NULL,
+                    done BOOLEAN DEFAULT FALSE,
+                    user_id INTEGER REFERENCES users(id),
+                    category_id INTEGER REFERENCES categories(id)
+                );
+            """)
 
     except Exception as e:
         print(f"Error while creating schema: {e}")
